8f9328c66b1514046122c0ba6ee6687822af961d
git-ly <yangli7715@sina.com>
2023-02-21 21:50:22 +0800

    code commit

--- src/test/java/org/example/ConfigServiceApplicationTests.java
+++ src/test/java/org/example/ConfigServiceApplicationTests.java
@@ -1,32 +1,32 @@
-package org.example;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ConfigServiceApplicationTests {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    void contextLoads(){
-
-    }
-
-    @Test
-    @Disabled("You can enable this test if your config-repo is publicly available")
-    void defaultConfigurationAvailable(){
-        ResponseEntity<Environment> entity = restTemplate
-                .getForEntity("/application/default",Environment.class);
-        Assertions.assertThat(entity.getStatusCode())
-                .isEqualTo(HttpStatus.OK);
-    }
-}
+//package org.example;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.core.env.Environment;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class ConfigServiceApplicationTests {
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Test
+//    void contextLoads(){
+//
+//    }
+//
+//    @Test
+//    @Disabled("You can enable this test if your config-repo is publicly available")
+//    void defaultConfigurationAvailable(){
+//        ResponseEntity<Environment> entity = restTemplate
+//                .getForEntity("/application/default",Environment.class);
+//        Assertions.assertThat(entity.getStatusCode())
+//                .isEqualTo(HttpStatus.OK);
+//    }
+//}
